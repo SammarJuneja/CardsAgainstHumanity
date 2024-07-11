0650c28531aeb53a41b2b20defd36a2651cf233e
SammarJuneja <sammarkumar345@gmail.com>
2024-07-11 15:39:22 +0530

    Add home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,50 @@
 <script lang="ts">
     import "../app.css";
-    import { route } from "../stores/store";
-  </script>
 
+    let mainOpen: boolean = true;
+    let createOpen: boolean;
+    let joinOpen: boolean;
+</script>
 
-<h2>hello world</h2>
+<div class="bg-gray-400 grid items-center min-h-screen">
+  {#if createOpen}
+    <div class="grid bg-yellow-600 p-2 mx-60 gap-2 rounded-md justify-center items-center">
+      <span class="flex">
+        <input placeholder="Your Name" class="bg-transparent placeholder-black pt-1 outline-none border-b border-yellow-700" type="text">
+        <button on:click={() => {
+          createOpen = !createOpen;
+          mainOpen = !mainOpen;
+          }}>X</button>
+      </span>
+      <input placeholder="Game Password" class="bg-transparent placeholder-black pt-1 outline-none border-b border-yellow-700" type="text">
+      <button class="bg-yellow-400 rounded-md">Create game</button>
+    </div>
+  {/if}
+  {#if joinOpen}
+    <div class="grid bg-yellow-600 p-2 mx-60 gap-2 rounded-md justify-center items-center">
+      <span class="flex">
+        <input placeholder="Your Name" class="bg-transparent placeholder-black pt-1 outline-none border-b border-yellow-700" type="text">
+        <button on:click={() => {
+          joinOpen = !joinOpen;
+          mainOpen = !mainOpen;
+          }}>X</button>
+      </span>
+      <input placeholder="Game Password" class="bg-transparent placeholder-black pt-1 outline-none border-b border-yellow-700" type="text">
+      <button class="bg-yellow-400 rounded-md">Join game</button>
+    </div>
+  {/if}
+  {#if mainOpen}
+  <div class="grid items-center justify-center text-center bg-yellow-600 mx-60 p-3 gap-2 rounded-md">
+    <button on:click={() => {
+      createOpen = !createOpen;
+      mainOpen = !mainOpen;
+    }} class="bg-yellow-400 p-1 rounded-md">Create game</button>
+    <button on:click={() => {
+      joinOpen = !joinOpen;
+      mainOpen = !mainOpen;
+    }} class="bg-yellow-400 p-1 rounded-md">Join game</button>
+    <a href="/room" class="bg-yellow-400 p-1 rounded-md">Bypass</a>
+  </div>
+  
+  {/if}
+</div>
---
+++ src/routes/room/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+</script>
+
+<div class="min-h-screen bg-gray-400">
+    <div>
+        <!-- black card -->
+    </div>
+    <div>
+        <!-- white card -->
+    </div>
+    <div class="bg-neutral-800 flex gap-2 bottom-0 fixed w-full text-white p-2">
+        <h2>hello</h2>
+        <h2>hello</h2>
+        <h2>hello</h2>
+    </div>
+</div>
