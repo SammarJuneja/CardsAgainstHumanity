c5cb9147442aa39209a4cea5a897bbd740d3814c
SammarJuneja <sammarkumar345@gmail.com>
2024-07-11 16:24:04 +0530

    Add cards json

--- src/routes/room/+page.svelte
+++ src/routes/room/+page.svelte
@@ -1,5 +1,35 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+    import "../../app.css";
+    import cah from "../../assets/cah.json";
+    
+    function getWhiteCrads() {
+        let white = cah[0].white;
+        let array: any[] = [];
+        let ind = new Set<number>();
+        let random = Math.min(10, white.length);
+        while (array.length < random) {
+            const index: any = Math.floor(Math.random() * white.length);
+            if (!ind.has(index)) {
+                array.push(white[index]);
+                ind.add(index);   
+            }
+        }
+         return array;
+    }
+    function getBlackCrads() {
+        let black = cah[0].black;
+        const random = Math.floor(Math.random() * black.length);
+        let result: any = black[random];
+        return result;
+    }
 
+    onMount(() => {
+        let ok = getWhiteCrads();
+        let lp = getBlackCrads()
+        console.log(ok, lp);
+    })
+    
 </script>
 
 <div class="min-h-screen bg-gray-400">
